5770f2e05f3d36a0664371da56e66da2038bd17f
Bram Vernimmen <vernimmenbram@gmail.com>
2023-04-04 21:12:00 +0200

    Cleaned up UI
    Clean up the UI of the OverViewPage a bit. Nearly final.

---
+++ YuGiOh/View/Converters/NullToInvisibleConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace YuGiOh.View.Converters
+{
+    //https://stackoverflow.com/questions/5123815/how-to-display-a-default-value-for-wpf-listbox-bound-to-an-empty-list
+    public class NullToInvisibleConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (int)value > 0 ? Visibility.Collapsed : Visibility.Visible;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
